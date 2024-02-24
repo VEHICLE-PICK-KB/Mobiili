3d3671cc4275160ed38c3b2f9b3e97e6475a30da
VEHICLE-PICK-KB <131762784+VEHICLE-PICK-KB@users.noreply.github.com>
2024-02-24 14:35:58 +0200

    Nykyinen sijainti + haku

---
+++ MAP2/App.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from 'react';
+import { StatusBar, Button, TextInput, View, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+
+export default function App() {
+  const [address, setAddress] = useState('');
+  const [location, setLocation] = useState(null);
+  const [coordinates, setCoordinates] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221,
+  });
+
+  useEffect(() => {
+    (async () => {
+      await getLocationAsync();
+    })();
+  }, []);
+
+  const getLocationAsync = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert('No permission to get location');
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    setCoordinates(prevState => ({
+      ...prevState,
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude
+    }));
+  };
+
+
+  const handleShow = () => {
+    fetch(`https://geocode.maps.co/search?q=${(address)}&api_key=65d9d60f3b280028582802dmj278e21`)
+      .then(response => response.json())
+      .then(data => {
+        if (data && data.length > 0) {
+          const firstResult = data[0];
+          const { lat, lon } = firstResult;
+          setCoordinates({
+            latitude: parseFloat(lat),
+            longitude: parseFloat(lon),
+            latitudeDelta: 0.0322,
+            longitudeDelta: 0.0221,
+          });
+        } else {
+          console.error("Ei koordinaatteja saatavilla");
+        }
+      })
+      .catch(error => console.error(error));
+  };
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.mapstyle}
+        region={coordinates}
+      >
+        <Marker
+          title="Sijainti"
+          coordinate={{ latitude: coordinates.latitude, longitude: coordinates.longitude }}
+        />
+      </MapView>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Syötä osoite"
+          value={address}
+          onChangeText={text => setAddress(text)}
+        />
+        <Button title="Show" onPress={handleShow} />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  mapstyle: {
+    flex: 1,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  input: {
+    flex: 1,
+    marginRight: 10,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+  },
+});
